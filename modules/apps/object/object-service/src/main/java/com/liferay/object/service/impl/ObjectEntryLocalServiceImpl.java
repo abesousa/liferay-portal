b4d2ad47028121555dd1b94af280186f967d6c25
Alberto Sousa <alberto.sousa@liferay.com>
2025-06-06 15:44:45 -0300

    LPD-57500 Add groupId to incomplete entry

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -461,12 +461,12 @@ public class ObjectEntryLocalServiceImpl
 
 	@Override
 	public ObjectEntry addObjectEntry(
-			String externalReferenceCode, long userId,
+			String externalReferenceCode, long groupId, long userId,
 			ObjectDefinition objectDefinition, long objectEntryFolderId)
 		throws PortalException {
 
 		return _addObjectEntry(
-			externalReferenceCode, userId,
+			externalReferenceCode, groupId, userId,
 			objectDefinition.getObjectDefinitionId(), objectEntryFolderId,
 			WorkflowConstants.STATUS_DRAFT);
 	}
@@ -1217,7 +1217,8 @@ public class ObjectEntryLocalServiceImpl
 	@Override
 	@Transactional(propagation = Propagation.REQUIRED)
 	public ObjectEntry getOrAddIncompleteObjectEntry(
-			String externalReferenceCode, long userId, long objectDefinitionId)
+			String externalReferenceCode, long groupId, long userId,
+			long objectDefinitionId)
 		throws PortalException {
 
 		ObjectEntry objectEntry = fetchObjectEntry(
@@ -1240,7 +1241,7 @@ public class ObjectEntryLocalServiceImpl
 		}
 
 		objectEntry = _addObjectEntry(
-			externalReferenceCode, userId, objectDefinitionId,
+			externalReferenceCode, groupId, userId, objectDefinitionId,
 			ObjectEntryFolderConstants.PARENT_OBJECT_ENTRY_FOLDER_ID_DEFAULT,
 			WorkflowConstants.STATUS_INCOMPLETE);
 
@@ -2331,14 +2332,16 @@ public class ObjectEntryLocalServiceImpl
 	}
 
 	private ObjectEntry _addObjectEntry(
-			String externalReferenceCode, long userId, long objectDefinitionId,
-			long objectEntryFolderId, int status)
+			String externalReferenceCode, long groupId, long userId,
+			long objectDefinitionId, long objectEntryFolderId, int status)
 		throws PortalException {
 
 		ObjectEntry objectEntry = objectEntryPersistence.create(
 			counterLocalService.increment());
 
 		objectEntry.setExternalReferenceCode(externalReferenceCode);
+
+		objectEntry.setGroupId(groupId);
 
 		User user = _userLocalService.getUser(userId);
 
