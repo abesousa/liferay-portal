b97df57a6408821603ff03cddc9f265bea6aa6d4
Alberto Sousa <alberto.sousa@liferay.com>
2026-01-06 13:56:56 -0300

    LPD-75529 List entries

---
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/view/table/TeamsTableFDSView.java
@@ -0,0 +1,42 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cms.site.initializer.internal.frontend.data.set.view.table;
+
+import com.liferay.frontend.data.set.view.FDSView;
+import com.liferay.frontend.data.set.view.table.FDSTableSchema;
+import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilder;
+import com.liferay.frontend.data.set.view.table.FDSTableSchemaBuilderFactory;
+import com.liferay.site.cms.site.initializer.internal.constants.CMSSiteInitializerFDSNames;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import java.util.Locale;
+
+/**
+ * @author Alberto Sousa
+ */
+@Component(
+	property = "frontend.data.set.name=" + CMSSiteInitializerFDSNames.VIEW_TEAMS,
+	service = FDSView.class
+)
+public class TeamsTableFDSView extends BaseCMSTableFDSView {
+	@Override
+	public FDSTableSchema getFDSTableSchema(Locale locale) {
+		FDSTableSchemaBuilder fdsTableSchemaBuilder =
+			fdsTableSchemaBuilderFactory.create();
+
+		return fdsTableSchemaBuilder.add(
+			"embedded.title", "title"
+		).add(
+			"embedded.description", "description"
+		).add(
+			"embedded.size", "size"
+		).build();
+	}
+
+	@Reference
+	protected FDSTableSchemaBuilderFactory fdsTableSchemaBuilderFactory;
+}
